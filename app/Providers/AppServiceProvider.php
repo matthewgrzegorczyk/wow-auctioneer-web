c9aa8d3378345ee2dcc325d2f08bec69e7bd4db0
Matthew Grzegorczyk <matthewgrzegorczyk@gmail.com>
2024-05-22 13:15:17 +0200

    Register auctioneer service provider as singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Auctioneer\Auctioneer;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(
+            Auctioneer::class,
+            function ($app) {
+                return new Auctioneer();
+            }
+        );
     }
 
     /**
