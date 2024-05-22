65e21e9a5f34e02a0a96729bd4cddba6629150f8
Matthew Grzegorczyk <matthewgrzegorczyk@gmail.com>
2024-05-22 13:01:47 +0200

    Install bootstrap / chartjs and dayjs to display sweet graphs on the frontend

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+            '@bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
